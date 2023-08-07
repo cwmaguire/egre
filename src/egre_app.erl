8f2a5625b8a6cf9384cb2d3b4755c1e7661f830e
Chris Maguire <cwmaguire@protonmail.com>
2023-08-07 11:19:28 -0700

    Remove Cowboy startup.
    After splitting EGRE out from GERLSHMUD, EGRE now *only* handles graph
    logic. The MUD engine, EGRE_MUD, will handle all the connections to the
    outside world with the web server.

--- src/egre_app.erl
+++ src/egre_app.erl
@@ -2,31 +2,15 @@
 -module(egre_app).
 -behaviour(application).
 
--include("include/egre.hrl").
+-include("egre.hrl").
 
 -export([start/2]).
 -export([stop/1]).
 
--define(ANY_HOST, '_').
--define(NO_OPTIONS, []).
 
 start(_Type, _Args) ->
     setup_and_or_start_mnesia(),
-    Port = case application:get_env(egre, port) of
-               {ok, EnvPort} ->
-                   EnvPort;
-               _ ->
-                   8080
-           end,
 
-    Paths = [{"/", egre_websocket, ?NO_OPTIONS},
-             {"/log", egre_websocket_log, ?NO_OPTIONS},
-             {"/[...]", cowboy_static, {priv_dir, egre, "static"}}],
-    Routes = [{?ANY_HOST, Paths}],
-    Dispatch = cowboy_router:compile(Routes),
-    _ = cowboy:start_clear(egre_http_listener,
-                           [{port, Port}],
-                           #{env => #{dispatch => Dispatch}}),
     egre_sup:start_link().
 
 stop(_State) ->
