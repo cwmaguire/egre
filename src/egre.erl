4e52d3e11e9f5ee04ce733ff471b3fdda4834cea
Chris Maguire <cwmaguire@protonmail.com>
2024-10-19 17:56:38 -0700

    Fleshed out the egre API
    Dependent libraries should not know anything about the internals of
    EGRE. EGRE should expose all necessary functions in one place to keep
    the API surface area small.

--- src/egre.erl
+++ src/egre.erl
@@ -1,12 +1,34 @@
 %% Copyright 2022, Chris Maguire <cwmaguire@protonmail.com>
 -module(egre).
 
+-include("egre.hrl").
+
 -export([create_graph/1]).
+-export([get_object/1]).
+-export([get_object_pid/1]).
+-export([start_object/1]).
+-export([attempt/2]).
 
 create_graph(Objects) ->
     IdPids = [{Id, start_obj(Id, Props)} || {Id, Props} <- Objects],
     [egre_object:populate(Pid, IdPids) || {_, Pid} <- IdPids],
     IdPids.
+
+-spec start_object(proplist()) -> pid().
+start_object(Properties) ->
+    supervisor:start_child(egre_object_sup, [_Id = undefined, Properties]).
+
+-spec get_object(pid()) -> #object{}.
+get_object(Pid) ->
+    egre_index:get(Pid).
+
+-spec get_object_pid(atom()) -> pid().
+get_object_pid(Id) ->
+    egre_index:get_pid(Id).
+
+-spec attempt(pid(), tuple()) -> any().
+attempt(ObjectPid, Event) ->
+    egre_object:attempt(ObjectPid, Event).
 
 start_obj(Id, Props) ->
     {ok, Pid} = supervisor:start_child(egre_object_sup, [Id, Props]),
