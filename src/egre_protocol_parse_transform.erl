54ff982e4fb78a709e6b97bb030fcb0e73000dde
Chris Maguire <cwmaguire@protonmail.com>
2024-10-31 14:58:21 -0700

    Fix parse transform attempt catch-all clause
    I already had a catch-all case for the attempt, but it was limited to a
    couple specific variables. I've changed it so that if the whole attempt
    (Parents, Props, Event) is captured by a single variable, then we just
    ignore it. I don't think I have any cases where it would make sent *not*
    to match inside the attempt argument tuple to access at least the props
    and the event.

--- src/egre_protocol_parse_transform.erl
+++ src/egre_protocol_parse_transform.erl
@@ -70,14 +70,10 @@ clause(_Name, {clause, _Line, _Head, _GuardGroups, Body}, State) ->
 attempt_clause({clause, _Line1, [{var, _Line2, '_'}], _, _}, _State) ->
     [];
 
+%% We don't need to see catch-all clauses in the protocol.
 %% Sometimes we'll catch anything that falls through in order to output
 %% missed events. We can ignore these.
 attempt_clause({clause, _Line1, [{var, _Line2, _Var}], _, _}, _State) ->
-    [];
-
-%% We don't need to see catch-all clauses in the protocol
-%% attempt(_Var) -> ...
-attempt_clause({clause, _Line1, [{var, _Line2, Var}], _, _}, _State) when Var == '_Attempt'; Var == '_Msg' ->
     [];
 
 %% We don't need to see catch-all clauses in the protocol
